a47150838d47204ebc89ecb5388ecbb67cee85aa
Chayce <chayce.knaub@sstech.us>
2022-01-26 10:44:21 -0500

    added bug examples

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+console.log("-----Primitive Types-----");
 let firstName: string = "Jane";
 
 let lastName: string = "Smalley";
@@ -7,7 +8,11 @@ let currentAge: number = 45;
 let isHappy: boolean = false;
 
 console.log(firstName);
-
+//
+//
+//
+//
+//
 console.log("-----Functions-----");
 
 // ES6 arrow function
@@ -16,13 +21,20 @@ const fullName = (first: string, last: string): string => {
 };
 
 console.log(fullName("Chayce", "Knaub"));
-
+//
+//
+//
+//
 //using function keyword
 function nameAndAge(first: string, last: string, age: number): string {
 	return `${first} ${last} is ${age.toString()} years old`;
 }
 console.log(nameAndAge(firstName, lastName, currentAge));
-
+//
+//
+//
+//
+//
 const happyOrNot = (name: string, happiness: boolean) => {
 	return happiness === true ? `${name} is happy.` : `${name} is not happy.`;
 };
@@ -30,13 +42,43 @@ const happyOrNot = (name: string, happiness: boolean) => {
 console.log(happyOrNot(firstName, isHappy));
 
 console.log(happyOrNot("Chayce", false));
-
+//
+//
+//
+//
+//
 //Arrays
 
-const customers = ["John Smith", "Jane Doe", "Marge Simpson"];
+const customers: string[] = ["John Smith", "Jane Doe", "Marge Simpson"];
+const numArray: number[] = [4, 56, 78, 32];
 
 customers.forEach((name) => {
 	console.log(name.toUpperCase());
 });
 
 customers.push("Alexander Hamilton");
+//
+//
+//
+//
+// TUPLES , an array with a specific mandatory structure
+let nameAge: [string, number] = ["Sally", 10];
+
+//Error
+//nameAge = [10, "Sally"]
+console.log(nameAge);
+//
+//
+//
+//
+// UNIONS
+
+function printId(id: number | string) {
+	console.log("Your ID is: " + id);
+}
+// OK
+printId(101);
+// OK
+printId("202");
+// Error
+//printId({ myID: 22342 });
--- vanilla.js
+++ vanilla.js
@@ -4,10 +4,27 @@ const user = {
 	isHungry: false,
 };
 
+//typo error
 console.log(user.haircolor);
-
+//
+//
+//
+//LOGIC ERRORS
 const hungryOrNot = (name, hungry) => {
-	return isHungry ? `${name} is hungry` : `${name} is not hungry`;
-};
+	// return hungry ? `${name} is hungry` : `${name} is not hungry`;
 
+	if (hungry) {
+		return `${name} is hungry`;
+	} else {
+		return `${name} is not hungry`;
+	}
+};
+//
+//
+// WORKING AS EXPECTED
+console.log(hungryOrNot("Michelle", true));
+console.log(hungryOrNot("Harsh", false));
+//NOT WORKING AS EXPECTED
 console.log(hungryOrNot("chayce"));
+
+console.log(hungryOrNot("Maggie", 0));
