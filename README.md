f93508ba2604bc75ae18cdb2a7f22ce0fe30b981
Chayce <chayce.knaub@sstech.us>
2022-01-26 10:52:36 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# TypeScript Notes
+
+**code examples**
+
+1. vanilla.js
+2. src/types.ts
+3. src/interfaces.ts
+
+**run JS files through index.html**
+
+1. vanilla.js
+2. dist/types.js
+3. dist/interfaces.js
--- vanilla.js
+++ vanilla.js
@@ -1,3 +1,4 @@
+// COMMON JAVASCRIPT BUGS
 const user = {
 	name: "James",
 	age: 28,
@@ -11,8 +12,10 @@ console.log(user.haircolor);
 //
 //LOGIC ERRORS
 const hungryOrNot = (name, hungry) => {
+	//ternary
 	// return hungry ? `${name} is hungry` : `${name} is not hungry`;
 
+	// if/else
 	if (hungry) {
 		return `${name} is hungry`;
 	} else {
